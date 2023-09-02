32fc6f738227f5dcc575ea4ec7b820ebe10051f7
Alisson <alissonalves29@gmail.com>
2023-09-02 11:52:38 -0300

    created Provide to authentication

---
+++ hooks/useUser.tsx
@@ -0,0 +1,36 @@
+import { User } from '@supabase/auth-helpers-nextjs';
+import { createContext, useState } from 'react';
+import { useSessionContext, useUser as useSupaUser } from '@supabase/auth-helpers-react';
+
+import { Subscription, UserDetails } from '@/types';
+
+type UserContextType = {
+    accessToken: string | null;
+    user: User | null;
+    userDetail: UserDetails | null;
+    isLoading: boolean;
+    subscription: Subscription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+    undefined
+)
+
+export interface Props {
+    [propName: string]: any;
+};
+
+export const MyUserContextProvider = (props: Props) => {
+    const {
+        session,
+        isLoading: isLoadingUser,
+        supabaseClient: supabase
+    } = useSessionContext();
+    const user = useSupaUser();
+    const accessToken = session?.access_token ?? null;
+    const [isLoadingData, setIsLoadingData] = useState(false);
+    const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+    const [subscription, setSubscription] = useState<Subscription | null>(null);
+
+    const getUserDetails = () => supabase.from('users').select('*').single();
+}
