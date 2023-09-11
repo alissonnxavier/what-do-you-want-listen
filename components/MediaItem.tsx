b60f951871690bd2c4e5215dfd4b8133ca6f21a4
Alisson <alissonalves29@gmail.com>
2023-09-10 21:04:19 -0300

    final

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 import Image from "next/image";
 
@@ -13,16 +14,18 @@ const MediaItem: React.FC<MediaItemProps> = ({
   data,
   onClick
 }) => {
-
+  const player = usePlayer();
   const imageUrl = useLoadImage(data);
 
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id);
     }
+
+    return player.setId(data.id);
   }
 
-  //TODO: default turn on player;
+  
 
   return (
     <div
